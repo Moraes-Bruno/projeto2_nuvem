605ad10c5f1a20343a006f2dd62b123f6dd51c3b
Bruno Silva <101658805+Moraes-Bruno@users.noreply.github.com>
2024-06-12 21:26:17 -0300

    Add files via upload

---
+++ pilotolojadsm/app/views/classes/home.php
@@ -0,0 +1,6 @@
+<?php
+    echo "<h1 class='msg'>Seja bem-vindo à nossa loja de roupas!</h1>";
+    echo "<h2 class='msg'>onde estilo e conforto se encontram para criar o seu visual perfeito!</h2>";
+    
+    echo'<script src="../js/scripts.js"></script>'
+?>
---
+++ pilotolojadsm/app/views/classes/produtos.php
@@ -0,0 +1,32 @@
+<?php
+    echo "<h2 class='subtitulo'>Nossos Produtos</h2>";
+    echo "<div class='produto'>";
+    echo "<img src='../img/polo.jpg'>";
+    echo '<div class="dados">';
+    echo "<h3>Camisa Polo</h3>";
+    echo "<p>Marca: Ralph Lauren</p>";
+    echo "<p>R$ 49,90</p>";
+    echo "<button class='add-carrinho' data-produto='Camisa Polo'>Adicionar ao Carrinho</button>";
+    echo'</div>';
+    echo "</div>";
+
+    echo "<div class='produto'>";
+    echo "<img src='../img/adidas.jpg'>";
+    echo '<div class="dados">';
+    echo "<h3>Tenis</h3>";
+    echo "<p>Marca: Adidas</p>";
+    echo "<p>R$ 300,00</p>";
+    echo "<button class='add-carrinho' data-produto='Camisa Polo'>Adicionar ao Carrinho</button>";
+    echo'</div>';
+    echo "</div>";
+
+    echo "<div class='produto'>";
+    echo "<img src='../img/diesel.jpg'>";
+    echo '<div class="dados">';
+    echo "<h3>Calça</h3>";
+    echo "<p>Marca: Diesel</p>";
+    echo "<p>R$ 500,00</p>";
+    echo "<button class='add-carrinho' data-produto='Camisa Polo'>Adicionar ao Carrinho</button>";
+    echo'</div>';
+    echo "</div>";
+?>
