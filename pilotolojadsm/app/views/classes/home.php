68d6a53e0272ff75477a1874eb4e9829543d5685
Bruno Silva <101658805+Moraes-Bruno@users.noreply.github.com>
2024-06-12 21:27:19 -0300

    Delete pilotolojadsm directory

--- pilotolojadsm/app/views/classes/home.php
+++
@@ -1,6 +0,0 @@
-<?php
-    echo "<h1 class='msg'>Seja bem-vindo à nossa loja de roupas!</h1>";
-    echo "<h2 class='msg'>onde estilo e conforto se encontram para criar o seu visual perfeito!</h2>";
-    
-    echo'<script src="../js/scripts.js"></script>'
-?>
